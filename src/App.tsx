e333012fb594904da5880d3e4a8ad8ea1395ea79
Lahiru Niroshan <e20272@eng.pdn.ac.lk>
2025-10-03 00:01:23 +0530

    Filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,11 @@ import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenres";
 import PlatformSelectors from "./components/PlatformSelectors";
+import { Platform } from "./hooks/useGames";
 
 function App() {
   const[selectedGenre,setSelectedGenre] = useState<Genre|null>(null);
+  const[selectedPlatform,setSelectedPlatform] = useState<Platform|null>(null);
 
   return (
     <Grid templateAreas={{
@@ -28,8 +30,8 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <PlatformSelectors/>
-        <GameGrid selectedGenre={selectedGenre}/>
+        <PlatformSelectors selectedPlatform = {selectedPlatform}  onSelectPlatform={(platform) =>setSelectedPlatform(platform)}/>
+        <GameGrid selectedPlatform={selectedPlatform} selectedGenre={selectedGenre}/>
       </GridItem>
     </Grid>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -4,7 +4,7 @@ import { CanceledError } from "axios";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
-export interface Platform{
+export interface Platform {
   id: number;
   name: string;
   slug: string;
@@ -14,12 +14,21 @@ export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms: {platform:Platform}[];
+  parent_platforms: { platform: Platform }[];
   metacritic: number;
 }
 
+const useGames = (
+  selectedGenre: Genre | null,
+  selectedPlatform: Platform | null
+) =>
+  useData<Game>(
+    "/games",
+    { params: { 
+      genres: selectedGenre?.id, 
+      platforms: selectedPlatform?.id 
+    }},
+    [selectedGenre?.id, selectedPlatform?.id]
+  );
 
-
-const useGames = (selectedGenre : Genre | null) => useData<Game>('/games', {params: {genres: selectedGenre?.id}},[selectedGenre?.id]);
-  
 export default useGames;
